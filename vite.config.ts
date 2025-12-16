78e444c5b2c4716946cdaef7d365c9419ad505af
conterGui <conterguilherme22@gmail.com>
2025-12-16 20:30:41 +0000

    edit vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "*", // ⚠️ para domínio próprio na raiz
+  base: "/", // ⚠️ para domínio próprio na raiz
   server: {
     host: "::",
     port: 8080,
